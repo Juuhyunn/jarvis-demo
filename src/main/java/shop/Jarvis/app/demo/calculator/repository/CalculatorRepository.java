6c3e37a6115230ab2da4affd9901e61f41f0a888
Juuhyunn <ahnju96@gmail.com>
2021-08-10 17:48:22 +0900

    first commit

---
+++ src/main/java/shop/Jarvis/app/demo/calculator/repository/CalculatorRepository.java
@@ -0,0 +1,9 @@
+package shop.Jarvis.app.demo.calculator.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import shop.Jarvis.app.demo.calculator.entity.Calculator;
+
+@Repository
+public interface CalculatorRepository extends MongoRepository<Calculator, Long> {
+}
---
+++ src/test/java/shop/Jarvis/app/demo/calculator/controller/CalculatorControllerTest.java
@@ -0,0 +1,11 @@
+package shop.Jarvis.app.demo.calculator.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorControllerTest {
+
+}
